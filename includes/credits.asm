71e3b889349b6398cd234f59a7655713dcc2664d
Esci <54251381+Esciii@users.noreply.github.com>
2019-12-16 17:56:04 -0500

    Add files via upload

---
+++ includes/credits.asm
@@ -0,0 +1,43 @@
+ .byte $0a
+ .byte $45
+ .byte $73
+ .byte $63
+ .byte $69
+ .byte $0a
+ .byte $45
+ .byte $73
+ .byte $63
+ .byte $69
+ .byte $23
+ .byte $32
+ .byte $30
+ .byte $30
+ .byte $34
+ .byte $0a
+ .byte $40
+ .byte $7a
+ .byte $6f
+ .byte $6b
+ .byte $6f
+ .byte $5f
+ .byte $68
+ .byte $68
+ .byte $68
+ .byte $68
+ .byte $0a
+ .byte $0a
+
+ .byte $32
+ .byte $33
+
+ .byte $2f
+
+ .byte $31
+ .byte $30
+
+ .byte $2f
+
+ .byte $31
+ .byte $39
+
+ .byte $0a
